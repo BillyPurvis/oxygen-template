2838335fcc6ec4822eb0cd500fb50316cf23e08e
Jasper Vinkenvleugel <jasper26v@gmail.com>
2015-04-04 13:50:39 +0200

    Reverted script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,9 @@
 window.onload = function() {
-    document.getElementById("background").style.height = (window.innerHeight + 100) + "px";
-
-    $("section").animate( {
-        paddingTop: "84px",
-        opacity: "1",
-    });
+  document.getElementById("background").style.height = (window.innerHeight + 100) + "px";
+  $("section").animate( {
+    paddingTop: "84px",
+    opacity: "1",
+  });
 };
 
 jQuery.easing.def = "easeOutExpo";
