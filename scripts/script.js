ca797456896aeffc2081897ad3ca863652248e6b
Jasper Vinkenvleugel <jasper26v@gmail.com>
2015-04-04 12:40:25 +0200

    Slight fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
 window.onload = function() {
-    document.getElementById("background").style.height= (window.innerHeight+100)+"px";
+  document.getElementById("background").style.height = (window.innerHeight + 100) + "px";
+  $("section").animate( {
+    paddingTop: "66px",
+    opacity: "1",
+  });
+};
 
-    $("section").animate( {
-        paddingTop: "66px",
-        opacity: "1",
-    });
+window.onresize = function() {
+  document.getElementById("background").style.height = (window.innerHeight + 100) + "px";
 };
 
 jQuery.easing.def = "easeOutExpo";
