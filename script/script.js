19a5b9323239463ce467ddb61fd95f45161c562e
Jasper Vinkenvleugel <jasper26v@gmail.com>
2015-05-06 13:14:19 +0200

    Updates and reorganization

--- script/script.js
+++ script/script.js
@@ -1,9 +1,14 @@
 window.onload = function() {
   document.getElementById("background").style.height = (window.innerHeight + 100) + "px";
+  
   $("section").animate( {
     paddingTop: "14px",
     opacity: "1",
   });
+
+  $("header.secondary").animate( {
+    opacity: "1",
+  }, 1000);
 };
 
 jQuery.easing.def = "easeOutExpo";
