848fdfa39ad4dbe023d696681c6c1725e4382054
Jasper Vinkenvleugel <jasper26v@gmail.com>
2015-05-06 13:46:37 +0200

    Added an animation to the subnav

--- script/base.js
+++ script/base.js
@@ -1,14 +1,16 @@
 window.onload = function() {
   document.getElementById("background").style.height = (window.innerHeight + 100) + "px";
-  
   $("section").animate( {
     paddingTop: "14px",
     opacity: "1",
   });
-
-  $("header.secondary").animate( {
+  $("header.secondaryfade").animate( {
     opacity: "1",
   }, 1000);
+};
+
+window.onresize = function() {
+  document.getElementById("background").style.height = (window.innerHeight + 100) + "px";
 };
 
 jQuery.easing.def = "easeOutExpo";
