23aa3f26b458fd0e87087df3c0d3b588a86bb94b
Jasper Vinkenvleugel <jasper26v@gmail.com>
2015-05-06 15:27:37 +0200

    Switched to CSS animations instead of JS

--- script/base.js
+++ script/base.js
@@ -1,12 +1,3 @@
 window.onload = function() {
   document.getElementById("background").style.height = (window.innerHeight + 100) + "px";
-  $("section").animate( {
-    paddingTop: "14px",
-    opacity: "1",
-  }, 1250);
-  $("header.secondaryfade").animate( {
-    opacity: "1",
-  }, 1250);
 };
-
-jQuery.easing.def = "easeOutExpo";
